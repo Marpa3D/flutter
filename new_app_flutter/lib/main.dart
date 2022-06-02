90788b0d82db9bfa5d3b9b7cc175bf7917c88b58
Viacheslav "Marpa3D" Sharov <marpa3d.evangelist@gmail.com>
2022-06-02 18:59:49 +0300

    Подключил Material Design & Scaffold

--- new_app_flutter/lib/main.dart
+++ new_app_flutter/lib/main.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Прямо в точку',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Прямо в точку'),
+        ),
+        body: const Center(
+          child: Text('Привет, это стартовый экран!)'),
+        ),
+      ),
+    ),
+  );
+}
